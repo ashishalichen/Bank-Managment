3501ff98e45842030d88476da47eb42bd42c7c77
nasrani-ad50 <68367001+nasrani-ad50@users.noreply.github.com>
2021-12-13 18:03:25 +0530

    Add files via upload

---
+++ Bank_F/mainmenu.java
@@ -0,0 +1,285 @@
+package Bank_F;
+
+import java.awt.Desktop;
+import java.awt.PopupMenu;
+import static java.awt.SystemColor.desktop;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+
+public class mainmenu extends javax.swing.JFrame 
+{
+
+    private PopupMenu internalFrame;
+
+
+    public mainmenu() 
+    {
+        initComponents();
+    }
+
+    
+    private void initComponents() 
+    {
+
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        jMenu4 = new javax.swing.JMenu();
+        jMenuItem7 = new javax.swing.JMenuItem();
+        jMenu5 = new javax.swing.JMenu();
+        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenu6 = new javax.swing.JMenu();
+        jMenuItem9 = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1366, 768));
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 823, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 764, Short.MAX_VALUE)
+        );
+
+        
+
+        jMenu2.setText("Transaction");
+
+        jMenuItem4.setText("Withdraw");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem4);
+
+        jMenuItem5.setText("Deposit");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem5);
+
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Transfer");
+        jMenu3.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenu3ActionPerformed(evt);
+            }
+        });
+
+        jMenuItem6.setText("AccountoAccount");
+        jMenuItem6.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem6ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jMenuItem6);
+
+        jMenuBar1.add(jMenu3);
+
+        jMenu4.setText("Report");
+
+        jMenuItem7.setText("Customer Report");
+        jMenuItem7.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem7ActionPerformed(evt);
+            }
+        });
+        jMenu4.add(jMenuItem7);
+
+        jMenuBar1.add(jMenu4);
+
+        jMenu5.setText("Balance");
+
+        jMenuItem8.setText("Balance Check");
+        jMenuItem8.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem8ActionPerformed(evt);
+            }
+        });
+        jMenu5.add(jMenuItem8);
+
+        jMenuBar1.add(jMenu5);
+
+        jMenu6.setText("Account");
+
+        jMenuItem9.setText("Account Creation");
+        jMenuItem9.addActionListener(new java.awt.event.ActionListener() 
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt) 
+            {
+                jMenuItem9ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem9);
+
+        jMenuBar1.add(jMenu6);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jDesktopPane1)
+                .addGap(69, 69, 69)
+                )
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jDesktopPane1)
+                .addContainerGap())
+        );
+
+        pack();
+    }
+
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+
+      customer cus = new customer();
+      jDesktopPane1.add(cus);
+      cus.setVisible(true);
+      
+    }
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        
+        account cus = new account();
+      jDesktopPane1.add(cus);
+      cus.setVisible(true);
+        
+        
+    }
+
+    private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        
+        withdraw cus = new withdraw();
+      jDesktopPane1.add(cus);
+      cus.setVisible(true);
+        
+    }
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        
+         deposit cus = new deposit();
+      jDesktopPane1.add(cus);
+      cus.setVisible(true);
+        
+    }
+
+    private void jMenu3ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+                 
+    }
+
+    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+         transfer cus1 = new transfer();
+             jDesktopPane1.add(cus1);
+             cus1.setVisible(true);
+        
+    }
+
+    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        cusreport cus1 = new cusreport();
+             jDesktopPane1.add(cus1);
+             cus1.setVisible(true);
+        
+        
+        
+    }
+
+    private void jMenuItem8ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        
+        
+        
+         balance cus1 = new balance();
+             jDesktopPane1.add(cus1);
+             cus1.setVisible(true);
+        
+    }
+
+    private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) 
+    {
+        
+        
+              user1 cus1 = new user1();
+             jDesktopPane1.add(cus1);
+             cus1.setVisible(true);
+        
+        
+        
+        
+    }
+
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
+                new mainmenu().setVisible(true);
+            }
+        });
+    }
+
+    
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
+    private javax.swing.JMenuItem jMenuItem7;
+    private javax.swing.JMenuItem jMenuItem8;
+    private javax.swing.JMenuItem jMenuItem9;
+    
+}
